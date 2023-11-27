0e1100fb4d6c407e83f83f67589b34b7fe4fe0ef
Max Dubrovin <max@cebb.pro>
2023-11-27 17:28:35 +0300

    fix: remove setup.py

--- setup.py
+++ setup.py
@@ -26,33 +26,34 @@ if AIO_COMPATIBLE:
 else:
     EXCLUDED_PACKAGES.append("keycloak.aio")
 
-setup(
-    name="python-keycloak-client",
-    version=VERSION,
-    long_description=README,
-    package_dir={"": "src"},
-    packages=find_packages("src", EXCLUDED_PACKAGES),
-    extras_require={
-        "dev": [
-            "bumpversion==0.5.3",
-            "twine",
-        ],
-        "doc": [
-            "Sphinx==1.4.4",
-            "sphinx-autobuild==0.6.0",
-        ],
-        "aio": ['aiohttp>=3.4.4,<4; python_full_version>="3.5.3"'],
-    },
-    setup_requires=["pytest-runner>=4.0,<5"],
-    install_requires=[
-        "requests",
-        "python-jose",
-    ],
-    tests_require=TESTS_REQUIRE,
-    url="https://github.com/MaxDXDX/python-keycloak-client",
-    license="MIT",
-    author="Peter Slump",
-    author_email="peter@yarf.nl",
-    description="Install Python Keycloak client.",
-    classifiers=[],
-)
+setup()
+# setup(
+#     name="python-keycloak-client",
+#     version=VERSION,
+#     long_description=README,
+#     package_dir={"": "src"},
+#     packages=find_packages("src", EXCLUDED_PACKAGES),
+#     extras_require={
+#         "dev": [
+#             "bumpversion==0.5.3",
+#             "twine",
+#         ],
+#         "doc": [
+#             "Sphinx==1.4.4",
+#             "sphinx-autobuild==0.6.0",
+#         ],
+#         "aio": ['aiohttp>=3.4.4,<4; python_full_version>="3.5.3"'],
+#     },
+#     setup_requires=["pytest-runner>=4.0,<5"],
+#     install_requires=[
+#         "requests",
+#         "python-jose",
+#     ],
+#     tests_require=TESTS_REQUIRE,
+#     url="https://github.com/MaxDXDX/python-keycloak-client",
+#     license="MIT",
+#     author="Peter Slump",
+#     author_email="peter@yarf.nl",
+#     description="Install Python Keycloak client.",
+#     classifiers=[],
+# )
